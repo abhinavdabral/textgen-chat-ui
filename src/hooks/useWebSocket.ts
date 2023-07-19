45fd5e5a812845d1b6d7c7c817f8b798d9e4b13a
Abhinav Dabral <abhinavdabral3k@gmail.com>
2023-07-20 02:28:30 +0530

    Auto-reconnect after disconnection

--- src/hooks/useWebSocket.ts
+++ src/hooks/useWebSocket.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useCallback } from "react";
 
 /**
  * Very simple hooks that initializes a websocket
@@ -12,31 +12,58 @@ export const useWebSocket = (
   handleMessage: (event: MessageEvent<any>) => void
 ) => {
   const socket = useRef<WebSocket | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
-  useEffect(() => {
+  const connect = useCallback(() => {
     const handleOpen = () => {
+      console.log("Connection successful.");
       setIsConnected(true);
+    };
+
+    const handleError = () => {
+      setIsConnected(false);
     };
 
     const handleClose = () => {
       setIsConnected(false);
+      console.log("Connection broken. Trying to reconnect in 1 second.");
+      cleanup();
+      timeoutRef.current = setTimeout(() => {
+        console.log("Trying to connect...");
+        timeoutRef.current = null;
+        connect();
+      }, 1000);
     };
-    if (url) {
-      socket.current = new WebSocket(url);
-      socket.current.addEventListener("open", handleOpen);
-      socket.current.addEventListener("close", handleClose);
-      socket.current.addEventListener("error", handleClose);
-      socket.current.addEventListener("message", handleMessage);
-    }
-    return () => {
-      if (socket.current && socket.current.readyState === WebSocket.OPEN) {
-        socket.current.close();
+
+    const cleanup = () => {
+      if (socket.current) {
+        if (socket.current.readyState === WebSocket.OPEN) {
+          socket.current.close();
+        }
         socket.current.removeEventListener("open", handleOpen);
         socket.current.removeEventListener("close", handleClose);
-        socket.current.removeEventListener("error", handleClose);
+        socket.current.removeEventListener("error", handleError);
         socket.current.removeEventListener("message", handleMessage);
       }
+    };
+
+    socket.current = new WebSocket(url);
+    socket.current.addEventListener("open", handleOpen);
+    socket.current.addEventListener("close", handleClose);
+    socket.current.addEventListener("error", handleError);
+    socket.current.addEventListener("message", handleMessage);
+
+    return cleanup;
+  }, []);
+
+  useEffect(() => {
+    const cleanup = connect();
+    return () => {
+      if (timeoutRef.current !== null) {
+        clearTimeout(timeoutRef.current);
+      }
+      cleanup();
     };
   }, []);
 
